20e48a6cc2b673ce21293ceea0d1fcbe020daf34
Nathan Yin <yinzeqi11@gmail.com>
2020-05-17 14:42:33 +1200

    Add count

--- guess.py
+++ guess.py
@@ -1,9 +1,11 @@
 import random
 r = random.randint(1, 100)
+count = 0
 
 while True:
 	num = input('请输入数字(范围1-100): ')
 	num = int(num)
+	count = count + 1
 
 	if num < r:
 		print('猜错了，数字猜小了！')
@@ -13,4 +15,5 @@ while True:
 
 	else:
 		print('恭喜你，猜对了！')
+		print('你一共猜了', count, '次')
 		break
